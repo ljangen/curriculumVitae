58b22fdf5ba546a956c4fac28e69f248488cfaac
ljangen <90193155+ljangen@users.noreply.github.com>
2022-11-22 15:12:08 +0100

    changed layout

--- cv/src/App.js
+++ cv/src/App.js
@@ -9,15 +9,15 @@ import ContactMe from './routes/ContactMe';
 function App() {
   return (
     <div className="App">
+      
       < NavBar />
 
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/AboutMe" element={<AboutMe />}/>
-        <Route path="/MyProjects" element={<MyProjects />}/>
-        <Route path="/ContactMe" element={<ContactMe />}/>
-
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/AboutMe" element={<AboutMe />}/>
+          <Route path="/MyProjects" element={<MyProjects />}/>
+          <Route path="/ContactMe" element={<ContactMe />}/>
+        </Routes>
     
     </div>
 
--- cv/src/routes/HomePage.jsx
+++ cv/src/routes/HomePage.jsx
@@ -3,8 +3,11 @@ import "./routes.css";
 const HomePage = () => {
     return ( 
         <div className="hero-section">
-            <div className="card">
-                <h2>Hi, my name is Christoffer and I am a fullstack developer. I specialize in the backend part (where I am fully educated)</h2>
+            <div className="card-container">
+                <div className="card">
+                    <h2> <span className="green-text">Christoffer</span> Ljung </h2> <br/>
+                    <h3>Fullstack <span className="green-text">Developer</span> </h3>
+                </div>
             </div>
         </div>
     );
